c63a9125c488c530f1ea159370fa2c5f2c493fec
Sheikh Danish <sdanish.gcp@gmail.com>
2017-09-26 12:45:08 +0530

    Aligment Chnges

--- src/main/java/com/javasampleapproach/angularjs/model/EmployeeRepository.java
+++ src/main/java/com/javasampleapproach/angularjs/model/EmployeeRepository.java
@@ -4,7 +4,6 @@ package com.javasampleapproach.angularjs.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Transactional
 public interface EmployeeRepository extends CrudRepository<Employee,Long>
 {
